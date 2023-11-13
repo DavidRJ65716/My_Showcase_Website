e55ff5e3bad0e347298fed7c937504f1404bac04
DavidRJ65716 <daverj65716@gmail.com>
2023-11-13 13:39:08 -0600

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import  { PageHeader }  from "./layouts/PageHeader"
+
+export default function App() {
+
+    return (
+        <div className="max-h-screen felx flex-col">
+            <PageHeader />
+            <div>2</div>
+        </div>
+    )
+}
+
