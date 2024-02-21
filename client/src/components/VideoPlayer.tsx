ece138ba0f67c473adc22a73dc02d6b446fff387
DavidRJ65716 <daverj65716@gmail.com>
2024-02-20 21:33:54 -0600

    Fixed volume slider accuracy

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,37 +1,54 @@
-import { Play, Pause, VolumeX, Volume, Volume1, Volume2 } from "lucide-react"
+import { Play, Pause, VolumeX, Volume1, Volume2 } from "lucide-react"
 import { Button } from "./Button"
 import { useEffect, useRef, useState } from "react"
 
 type VideoPlayerPops = {
     videoUrl: string
-    videotime: number
+    videoTime: number
 }
 
-export function VideoPlayer({videoUrl, videotime}:VideoPlayerPops) {
+export function VideoPlayer({videoUrl, videoTime = 0}:VideoPlayerPops) {
 
-    const [isVideoPlaying, setIsVideoPlaying] = useState(true)
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false)
     const [isContolShowing, setIsControlShowing] = useState(false)
-    const [isMuted, setIsMuted] = useState(true)
-    const [volumeLevel, setVolumeLevel] = useState(0.5)
+    const [isMuted, setIsMuted] = useState(false)
+    const [volumeLevel, setVolumeLevel] = useState(50)
+    const [sliderLevel, setSliderLevel] = useState(50)
     const videoRef = useRef<HTMLVideoElement>(null)
 
-    useEffect(()=>{
+    useEffect(()=> {
         
+        window.addEventListener("focus",onFocus)
+        videoPlayToggle()
+
+        return () => {
+            window.removeEventListener("focus",onFocus)
+        }
+
+    },[])
+
+    const onFocus = () =>{
+        console.log("tab is focus")
+    }
+
+    const videoPlayToggle = () => {
+        setIsVideoPlaying(v => !v)
         if (videoRef.current == null) return
 
         if (isVideoPlaying){
-            videoRef.current.currentTime = videotime
+            videoRef.current.currentTime = videoTime
             videoRef.current.play()
             setIsControlShowing(false)
         } else {
             videoRef.current.pause()
             setIsControlShowing(true)
         }
-        
-    },[isVideoPlaying])
+    }
 
-    const videoPlayToggle = () => {
-        setIsVideoPlaying(v => !v)
+    //0 to 100 are the only numbers in scope
+    const setVideoVolume = (newVolume: number) => {
+        if (videoRef.current == null) return
+        videoRef.current.volume = newVolume/100
     }
 
     const videoMuteToggle = () => {
@@ -39,18 +56,17 @@ export function VideoPlayer({videoUrl, videotime}:VideoPlayerPops) {
     }
 
     const volumeHandler = (newVolume: React.ChangeEvent<HTMLInputElement>) => {
-        if (videoRef.current == null) return
         setVolumeLevel(Number(newVolume.target.value))
-        videoRef.current.volume = volumeLevel
-        if (!isMuted && videoRef.current.volume === 0) {
-            //setVolumeLevel(0)
+        if (volumeLevel === 0) {
+            setSliderLevel(0)
             setIsMuted(true)
         } else {
             setIsMuted(false)
+            setSliderLevel(volumeLevel)
+            setVideoVolume(volumeLevel)
         }
     }
 
-    //group-hover:opacity-100 opacity-0 group-focus-within:opacity-100 max-w-screen-lx
     return (
 
         <div 
@@ -72,10 +88,10 @@ export function VideoPlayer({videoUrl, videotime}:VideoPlayerPops) {
                                 size={"player"}
                             >
                                 <Play className={`
-                                    ${isVideoPlaying && "hidden"}`}
+                                    ${!isVideoPlaying && "hidden"}`}
                                     color="white"
                                 />
-                                <Pause className={`${!isVideoPlaying && "hidden" }`} color="white"/>
+                                <Pause className={`${isVideoPlaying && "hidden" }`} color="white"/>
                             </Button>
                         </div>
                         <div className="group/volume flex flex-row ">{/*volume button*/}
@@ -85,22 +101,23 @@ export function VideoPlayer({videoUrl, videotime}:VideoPlayerPops) {
                                 size={"player"}
                             >
                                 <VolumeX className={`${!isMuted && "hidden"}`} color="white" />
-                                <Volume1 className={`${volumeLevel >= 0.5 && "hidden"} ${isMuted && "hidden"}`} color="white"/>
-                                <Volume2 className={`${volumeLevel < 0.5 && "hidden"}`} color="white"/>
+                                <Volume1 className={`${sliderLevel >= 50 && "hidden"} ${isMuted && "hidden"}`} color="white"/>
+                                <Volume2 className={`${sliderLevel < 50 && "hidden"} ${isMuted && "hidden"}`} color="white"/>
                             </Button>
                             <div className="flex items-center px-2 py-4 ">
                                 <input 
                                     className="w-0 origin-left scale-x-0 transition-transform group-hover/volume:w-[100px] group-hover/volume:scale-x-100"
                                     type="range"
                                     min="0"
-                                    max="1"
-                                    step="any"
-                                    value={volumeLevel}
+                                    max="100"
+                                    
+                                    value={sliderLevel}
                                     onChange={volumeHandler}
                                 />
                             </div>
+                            
                         </div>
-                        <div> {/*time duration*/}
+                        <div className="flex items-center flex-row"> {/*time duration*/}
                             <p>time</p>
                         </div>
                     </div>
@@ -122,5 +139,6 @@ export function VideoPlayer({videoUrl, videotime}:VideoPlayerPops) {
                 onClick={videoPlayToggle}
             />
         </div>
+
     )
 }
